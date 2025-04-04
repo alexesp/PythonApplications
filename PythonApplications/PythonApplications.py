5c2bbfecc6941e3c146473ad788236b1cd13082b
alexesp <alexizaes@hotmail.com>
2025-04-04 13:24:51 +0200

    Repaso ficheros

--- PythonApplications/PythonApplications.py
+++ PythonApplications/PythonApplications.py
@@ -1,3 +1,6 @@
+from token import OP
+
+
 print("Repaso de sintaxis en Python")
 
 def greet():
@@ -97,3 +100,30 @@ print(student_two.average())
 print(Student.average(student_two))
 
 
+print("#########################################################")
+
+class WorkingStudent(Student):
+    def __init__(self, new_name, new_grade, salary):
+        super().__init__(new_name, new_grade)
+        self.salary = salary
+    @property
+    def weekly_salary(self):
+        return self.salary * 15
+
+rolf = WorkingStudent('Rolf',[70,80,90,99], 1500)
+print(rolf.salary)
+print(rolf.weekly_salary)
+
+
+print("#########################################################")
+
+my_file = open('data.txt', 'r')
+file_content = my_file.read()
+
+my_file.close()
+print(file_content)
+
+user_name = input('Enter your name: ')
+my_file = open('write.txt', 'w')
+my_file.write(user_name)
+my_file.close()
